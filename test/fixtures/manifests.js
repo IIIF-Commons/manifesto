254664f0c062119edf9bbe54b7251d175e0c39c1
edsilv <e.silverton@gmail.com>
2015-09-14 17:28:33 +0100

    added manifests

--- test/fixtures/manifests.js
+++ test/fixtures/manifests.js
@@ -8,3 +8,11 @@ module.exports = {
     "chemistdruggist": "http://localhost:3000/chemistdruggist.json",
     "horriblemurders": "http://localhost:3000/horriblemurders.json"
 };
+
+//BBoM: http://wellcomelibrary.org/iiif/b18031511/manifest
+//2 copies: http://wellcomelibrary.org/iiif/b10111037/manifest
+//8 copies: http://wellcomelibrary.org/iiif/b18032096/manifest
+//8 volumes: http://wellcomelibrary.org/iiif/b21300185/manifest
+
+//- https://github.com/IIIF/iiif.io/blob/master/source/api/presentation/2.0/example/errors/collection.json
+//A collection of erroneous manifests
