b45965ea883c478a7efe0d01e8d0b2d4c67857e7
edsilv <e.silverton@gmail.com>
2016-06-03 12:52:06 +0100

    added comment

--- tasks/concat.js
+++ tasks/concat.js
@@ -4,11 +4,12 @@ var config = new c();
 var gulp = require('gulp');
 var merge = require('merge2');
 
+// todo: create a manifesto.bundle.js build instead of bundling these by default
 gulp.task('concat', function(cb) {
     var client = config.client + '/' + config.jsOut;
     var server = config.server + '/' + config.jsOut;
     var exjs = './bower_components/exjs/dist/ex.es3.min.js';
-    var extensions = './bower_components/extensions/dist/extensions.js'; // todo: is the whole lib needed?
+    var extensions = './bower_components/extensions/dist/extensions.js';
     
     return merge([
         gulp.src([exjs, extensions, client])
