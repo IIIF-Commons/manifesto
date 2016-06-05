02b1dac4d8c0bc8fcc248ff964d2278407f56368
edsilv <e.silverton@gmail.com>
2016-06-05 12:50:57 +0100

    remove unneeded minifications

--- tasks/minify.js
+++ tasks/minify.js
@@ -6,9 +6,7 @@ var utils = require('gulp-utils');
 gulp.task('minify', function(cb){
     Promise.all([
         utils.minify(config.client + '/' + config.jsOut, config.client),
-        utils.minify(config.client + '/' + config.jsMinOut, config.client),
-        utils.minify(config.server + '/' + config.jsOut, config.server),
-        utils.minify(config.server + '/' + config.jsMinOut, config.server),
+        utils.minify(config.server + '/' + config.jsOut, config.server)
     ]).then(function(){
         cb();
     });
