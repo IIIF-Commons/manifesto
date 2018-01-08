88be2d6b22f51b3f48f89c475aa3bd0b6578cefc
edsilv <e.silverton@gmail.com>
2018-01-08 17:50:24 +0000

    remove docs task until typedoc fixed

--- dist/manifesto.d.ts
+++ dist/manifesto.d.ts
@@ -1,4 +1,4 @@
-// manifesto v2.2.6 https://github.com/iiif-commons/manifesto
+// manifesto v2.2.7 https://github.com/iiif-commons/manifesto
 
 declare namespace Manifesto {
     class StringValue {
