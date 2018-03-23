336a3515ab1bd7a2d26a83d5f43df07b62610637
edsilv <e.silverton@gmail.com>
2018-03-23 17:53:28 +0000

    added test for items instead of sequence

--- dist/manifesto.d.ts
+++ dist/manifesto.d.ts
@@ -1,4 +1,4 @@
-// manifesto v2.2.11 https://github.com/iiif-commons/manifesto
+// manifesto v2.2.13 https://github.com/iiif-commons/manifesto
 
 declare namespace Manifesto {
     class StringValue {
