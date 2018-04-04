8500049d7ead7d00666cad92e978e32a14b131ce
edsilv <e.silverton@gmail.com>
2018-04-04 11:39:32 +0100

    build

--- dist/manifesto.d.ts
+++ dist/manifesto.d.ts
@@ -1,4 +1,4 @@
-// manifesto v2.2.14 https://github.com/iiif-commons/manifesto
+// manifesto v2.2.15 https://github.com/iiif-commons/manifesto
 
 declare namespace Manifesto {
     class StringValue {
