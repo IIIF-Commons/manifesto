ebd2b562125cb7dda486006d4092915b44f7a620
edsilv <e.silverton@gmail.com>
2018-06-25 14:49:19 +0100

    undo range.spansTime changes

--- dist/manifesto.d.ts
+++ dist/manifesto.d.ts
@@ -1,4 +1,4 @@
-// manifesto v2.2.26 https://github.com/iiif-commons/manifesto
+// manifesto v2.2.27 https://github.com/iiif-commons/manifesto
 
 declare namespace Manifesto {
     class StringValue {
