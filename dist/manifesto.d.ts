ea14f7454287266b0136453fba012e49443289cd
edsilv <e.silverton@gmail.com>
2018-07-12 16:33:36 +0100

    added getPosterCanvas

--- dist/manifesto.d.ts
+++ dist/manifesto.d.ts
@@ -1,4 +1,4 @@
-// manifesto v2.2.29 https://github.com/iiif-commons/manifesto
+// manifesto v2.2.30 https://github.com/iiif-commons/manifesto
 
 declare namespace Manifesto {
     class StringValue {
