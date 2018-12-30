f36d5f1458c4e748472678212ae741b4546b6d52
edsilv <e.silverton@gmail.com>
2018-12-30 10:38:20 +0000

    changed dist directory names, use 'barrel' syntax for index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { IManifestoOptions } from "./IManifestoOptions";
+import { Deserialiser } from "./Serialisation";
+import { Utils } from "./Utils";
+
 // import { 
 //     AnnotationMotivation as AnnotationMotivationObj, 
 //     Behavior as BehaviorObj,
@@ -9,16 +13,16 @@
 //     ViewingDirection as ViewingDirectionObj, 
 //     ViewingHint as ViewingHintObj
 // } from "@iiif/vocabulary";
-import { Annotation as AnnotationObj } from "./Annotation";
+// import { Annotation as AnnotationObj } from "./Annotation";
 // import { AnnotationBody as AnnotationBodyObj } from "./AnnotationBody";
 // import { AnnotationList as AnnotationListObj } from "./AnnotationList";
 // import { AnnotationPage as AnnotationPageObj } from "./AnnotationPage";
 // import { Canvas as CanvasObj } from "./Canvas";
 // import { Collection as CollectionObj } from "./Collection";
-import { Deserialiser as DeserialiserObj } from "./Serialisation";
+// import { Deserialiser as DeserialiserObj } from "./Serialisation";
 // import { Duration as DurationObj } from "./Duration";
 // import { IIIFResource as IIIFResourceObj } from "./IIIFResource";
-import { IManifestoOptions } from "./IManifestoOptions";
+// import { IManifestoOptions } from "./IManifestoOptions";
 // import { LabelValuePair as LabelValuePairObj } from "./LabelValuePair";
 // import { Language as LanguageObj } from "./Language";
 // import { LanguageMap as LanguageMapObj } from "./LanguageMap";
@@ -33,15 +37,86 @@ import { IManifestoOptions } from "./IManifestoOptions";
 // import { Thumbnail as ThumbnailObj } from "./Thumbnail";
 // import { TreeNode as TreeNodeObj } from "./TreeNode";
 // import { TreeNodeType as TreeNodeTypeObj } from "./TreeNodeType";
-import { Utils as UtilsObj } from "./Utils";
+// import { Utils as UtilsObj } from "./Utils";
 
-// declare global {
-//     const manifesto: any;
-// }
+export * from "@iiif/vocabulary";
+export * from "./Annotation";
+export * from "./AnnotationBody";
+export * from "./AnnotationList";
+export * from "./AnnotationPage";
+export * from "./Canvas";
+export * from "./Collection";
+export * from "./Serialisation";
+export * from "./Duration";
+export * from "./IIIFResource";
+export * from "./IManifestoOptions";
+export * from "./LabelValuePair";
+export * from "./Language";
+export * from "./LanguageMap";
+export * from "./Manifest";
+export * from "./ManifestType";
+export * from "./Range";
+export * from "./Sequence";
+export * from "./Service";
+export * from "./Size";
+export * from "./StatusCode";
+export * from "./Thumb";
+export * from "./Thumbnail";
+export * from "./TreeNode";
+export * from "./TreeNodeType";
+export * from "./Utils";
 
-(<any>global).manifesto = () => {
-    Annotation: typeof AnnotationObj;
+export const create = (manifest: string, options?: IManifestoOptions) => {
+    return Deserialiser.parse(manifest, options);
 }
+
+export const loadManifest = (uri: string) => {
+    return Utils.loadResource(uri);
+}
+
+// (<any>global).manifesto = () => {
+
+//     const Annotation: AnnotationObj = new AnnotationObj();
+//     AnnotationBody: AnnotationBodyObj;
+//     AnnotationList: AnnotationListObj;
+//     AnnotationMotivation: AnnotationMotivationObj;
+//     AnnotationPage: AnnotationPageObj;
+//     Behavior: BehaviorObj;
+//     Canvas: CanvasObj;
+//     Collection: CollectionObj;
+//     Duration: DurationObj;
+//     IIIFResource: IIIFResourceObj;
+//     IIIFResourceType: IIIFResourceTypeObj;
+//     LabelValuePair: LabelValuePairObj;
+//     Language: LanguageObj;
+//     LanguageMap: LanguageMapObj;
+//     Manifest: ManifestObj;
+//     ManifestType: ManifestTypeObj;
+//     MediaType: MediaTypeObj;
+//     Range: RangeObj;
+//     RenderingFormat: RenderingFormatObj;
+//     ResourceType: ExternalResourceTypeObj;
+//     Sequence: SequenceObj;
+//     Service: ServiceObj;
+//     ServiceProfile: ServiceProfileObj;
+//     Size: SizeObj;
+//     StatusCode: StatusCodeObj;
+//     Thumb: ThumbObj;
+//     Thumbnail: ThumbnailObj;
+//     TreeNode: TreeNodeObj;
+//     TreeNodeType: TreeNodeTypeObj;
+//     Utils: UtilsObj;
+//     ViewingDirection: ViewingDirectionObj;
+//     ViewingHint: ViewingHintObj;
+
+//     const create = (manifest: string, options?: IManifestoOptions) => {
+//         return DeserialiserObj.parse(manifest, options);
+//     }
+
+//     const loadManifest = (uri: string) => {
+//         return UtilsObj.loadResource(uri);
+//     }
+// }
 
 /*
 (<any>global).manifesto = {
@@ -83,10 +158,3 @@ import { Utils as UtilsObj } from "./Utils";
 }
 */
 
-(<any>global).create = (manifest: string, options?: IManifestoOptions) => {
-    return DeserialiserObj.parse(manifest, options);
-}
-
-(<any>global).loadManifest = (uri: string) => {
-    return UtilsObj.loadResource(uri);
-}
