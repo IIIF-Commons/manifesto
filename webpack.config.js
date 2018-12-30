feddbeb55731dee98d36e42e2af190f05c158832
edsilv <e.silverton@gmail.com>
2018-12-30 10:40:03 +0000

    comments

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ module.exports = {
         'manifesto': ['./src/index.ts'],
         'manifesto.min': ['./src/index.ts']
     },
-    // The output defines how and where we want the bundles. The special value `[name]` in `filename` tell Webpack to use the name we defined above.
-    // We target a UMD and name it MyLib. When including the bundle in the browser it will be accessible at `window.MyLib`
+    // The output defines how and where we want the bundles. The special value `[name]` in `filename` tells Webpack to use the name we defined above.
+    // We target a UMD and name it manifesto. When including the bundle in the browser it will be accessible at `window.manifesto`
     output: {
         path: path.resolve(__dirname, 'dist-umd'),
         filename: '[name].js',
