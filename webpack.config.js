0faa5f3aad6495b02c502e7adceb57f1d1af2045
edsilv <e.silverton@gmail.com>
2019-03-09 17:06:29 +0000

    use env vars

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
 
-const umdConfig = {
+function resolvePath(p) {
+    return path.resolve(__dirname, p)
+}
+
+const config = {
     // These are the entry point of our library. We tell webpack to use the name we assign later, when creating the bundle.
     // We also use the name to filter the second entry point for applying code minification via UglifyJS
     entry: {
@@ -9,7 +13,7 @@ const umdConfig = {
     // The output defines how and where we want the bundles. The special value `[name]` in `filename` tells Webpack to use the name we defined above.
     // We target a UMD and name it manifesto. When including the bundle in the browser it will be accessible at `window.manifesto`
     output: {
-        path: path.resolve(__dirname, 'dist-umd'),
+        path: resolvePath('dist-umd'),
         filename: '[name].js',
         libraryTarget: 'umd',
         library: 'manifesto',
@@ -39,43 +43,12 @@ const umdConfig = {
     }
 }
 
-const varConfig = {
-    // These are the entry point of our library. We tell webpack to use the name we assign later, when creating the bundle.
-    // We also use the name to filter the second entry point for applying code minification via UglifyJS
-    entry: {
-        'manifesto': ['./src/index.ts']
-    },
-    // The output defines how and where we want the bundles. The special value `[name]` in `filename` tells Webpack to use the name we defined above.
-    // We target a UMD and name it manifesto. When including the bundle in the browser it will be accessible at `window.manifesto`
-    output: {
-        path: path.resolve(__dirname, 'dist-var'),
-        filename: '[name].js',
-        libraryTarget: 'var',
-        library: 'manifesto',
-        umdNamedDefine: true
-    },
-    // Add resolve for `tsx` and `ts` files, otherwise Webpack would
-    // only look for common JavaScript file extension (.js)
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    // Activate source maps for the bundles in order to preserve the original
-    // source when the user debugs the application
-    devtool: 'source-map',
-    optimization: {
-        minimize: true
-    },
-    // Webpack doesn't understand TypeScript files and a loader is needed.
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: [
-                    { loader: 'awesome-typescript-loader' }
-                ]
-            }
-        ]
-    }
+if (process.env.NODE_WEBPACK_LIBRARY_PATH) {
+    config.output.path = resolvePath(process.env.NODE_WEBPACK_LIBRARY_PATH);
 }
 
-module.exports = [ umdConfig, varConfig ];
+if (process.env.NODE_WEBPACK_LIBRARY_TARGET) {
+    config.output.libraryTarget = process.env.NODE_WEBPACK_LIBRARY_TARGET;
+}
+
+module.exports = config;
