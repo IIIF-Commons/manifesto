e360e1f1c909f7a9f8e4d55265d96782dc835aef
Vincent Marchetti <vmarchetti@aluminum.local>
2024-02-15 11:33:45 -0500

    Implemented a complete -- for now -- set of tests for the model_origin manifest

--- test/tests/model_origin.js
+++ test/tests/model_origin.js
@@ -3,7 +3,13 @@ var should = require('chai').should();
 var manifesto = require('../../dist-commonjs/');
 var manifests_3d = require('../fixtures/manifests_3d');
 
-var manifest,  sequence, scene ;
+
+var ExternalResourceType = require('@iiif/vocabulary/dist-commonjs/').ExternalResourceType;
+var MediaType = require('@iiif/vocabulary/dist-commonjs/').MediaType;
+
+
+let manifest,  sequence, scene , annotation, body;
+
 
 describe('model_origin', function() {
 
@@ -22,11 +28,41 @@ describe('model_origin', function() {
     it('has a scene', function() {
         scene = sequence.getScenes()[0];
         expect(scene).to.exist;
-    });
-    
-    it('is a scene', function(){
         expect(scene.isScene());
     });
+    
+    
+    it('with one annotation', function(){
+        var annotations = scene.getContent();
+        expect(annotations.length).to.equal(1);
+        annotation = annotations[0];
+    });
+        
+    it('that target the scene', function(){        
+        var target = annotation.getTarget();
+        target.should.be.a('string');
+        target.should.equal( scene.id );
+    });
+    
+    it('and body is an AnnotationBody', function(){        
+        body = annotation.getBody();
+        if (Array.isArray(body)) body = body[0];
+
+        body.getType().should.equal(ExternalResourceType.MODEL);
+    });
+    
+    it('body id looks like a model url', function(){        
+        body.id.should.include('astronaut.glb');
+    });
+    
+    it('body Format (if defined) is glb or glTF', function(){
+        var mediaType = body.getFormat();
+        if (mediaType)
+            ['MediaType.GLB, MediaType.GLTF'].should.include(mediaType);
+    });
+        
+
+
 
 
 });
