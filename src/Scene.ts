4a813ce306e94d0d9932b6ccc1be1686cd223685
Vincent Marchetti <vmarchetti@aluminum.local>
2024-02-15 11:25:57 -0500

    implement getContent method (copied from Canvas type)

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,5 +1,7 @@
 
 import {
+  Annotation,
+  AnnotationPage,
   IManifestoOptions,
   ManifestResource,
 } from "./internal";
@@ -7,5 +9,36 @@ import {
 export class Scene extends ManifestResource {
   constructor(jsonld: any, options: IManifestoOptions) {
     super(jsonld, options);
+  }
+
+
+  // Presentation API 3.0
+  getContent(): Annotation[] {
+    const content: Annotation[] = [];
+
+    const items = this.__jsonld.items || this.__jsonld.content;
+
+    if (!items) return content;
+
+    // should be contained in an AnnotationPage
+    let annotationPage: AnnotationPage | null = null;
+
+    if (items.length) {
+      annotationPage = new AnnotationPage(items[0], this.options);
+    }
+
+    if (!annotationPage) {
+      return content;
+    }
+
+    const annotations: Annotation[] = annotationPage.getItems();
+
+    for (let i = 0; i < annotations.length; i++) {
+      const a = annotations[i];
+      const annotation = new Annotation(a, this.options);
+      content.push(annotation);
+    }
+
+    return content;
   }
 }
