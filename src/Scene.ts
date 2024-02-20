a07181132dd7af33b52b81bd70413bb199ee1662
Vincent Marchetti <vmarchetti@kshell.com>
2024-02-20 15:28:50 -0500

    implement the getBackgroundColor method

--- src/Scene.ts
+++ src/Scene.ts
@@ -37,8 +37,34 @@ export class Scene extends ManifestResource {
       const a = annotations[i];
       const annotation = new Annotation(a, this.options);
       content.push(annotation);
-    }
+    };
 
     return content;
-  }
+  };
+
+ 
+  getBackgroundColor() : object | undefined {
+    // regular expression intended to match strings like
+    // "#FF00FF" -- interpreted as three hexadecimal values
+    // in range 0-255 . Not that the \w escape matches digits,
+    // upper and lower case latin characters, and underscore
+    var hexColorFormat = new RegExp("#(\w{2})(\w{2})(\w{2})");
+    
+    var backgroundColorValue = this.__jsonld.backgroundColor;
+    console.log("backgroundColorValue " + backgroundColorValue);
+    if (backgroundColorValue && typeof(backgroundColorValue) == 'string')
+    {
+        var matchResult = backgroundColorValue.match(hexColorFormat);
+        console.log("matchResult " + matchResult);
+        if  (matchResult){
+            return {
+                red   : parseInt(matchResult[0],16),
+                green : parseInt(matchResult[1],16),
+                blue  : parseInt(matchResult[2],16)
+            }
+        }
+    }
+    return undefined;
+  };
+
 }
