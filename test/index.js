2eb19caba11d3fb0312152908d829596630fca22
Vincent Marchetti <vmarchetti@kshell.com>
2024-03-24 14:18:50 -0400

    Added testing of ambient_green_light

--- test/index.js
+++ test/index.js
@@ -114,6 +114,7 @@ else{
 }
 
 function run_iiif3d_tests(){
+    
     require('./tests_3d/core_tests/parse_manifest.js');
     require('./tests_3d/core_tests/class_color.js');
     describe("1_basic_model_in_scene" , function(){
@@ -125,6 +126,10 @@ function run_iiif3d_tests(){
         importTest('model_origin', './tests_3d/4_transform_and_position/model_position.js');
         importTest('model_transform_translate_rotate_position', './tests_3d/4_transform_and_position/model_transform_translate_rotate_position.js');
         importTest('model_transform_scale_position', './tests_3d/4_transform_and_position/model_transform_scale_position.js');        
+    });
+    
+    describe("3_lights" , function(){
+        importTest('ambient_green_light', './tests_3d/3_lights/ambient_green_light.js');
     });
 }
 
