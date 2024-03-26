ae428ce52f1c8892c9fa5d76468199b0eb37643b
Vincent Marchetti <vmarchetti@kshell.com>
2024-03-26 11:30:40 -0400

    Demonstrated that a threejs_math.Vector3 instance can be returned as a point selector location and used in X3DOM scene construction

--- src/PointSelector.ts
+++ src/PointSelector.ts
@@ -4,6 +4,8 @@ import {
 
 } from "./internal";
 
+import { Vector3 } from "threejs-math";
+
 
 export class PointSelector extends JSONLDResource  {
 
@@ -13,10 +15,13 @@ export class PointSelector extends JSONLDResource  {
     super(jsonld);
   }
   
-  getLocation() : any {
+  getLocation() : Vector3 {
+    return new Vector3(this.__jsonld.x, this.__jsonld.y, this.__jsonld.z );
+    /*
   	return { x:Number(this.__jsonld.x),
   	         y:Number(this.__jsonld.y),
   	         z:Number(this.__jsonld.z)
   	       }
+  	*/
   }
 }
