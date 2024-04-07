a60bc61dbdd23c3ebf6e210e2332e56ef90997e5
Vincent Marchetti <vmarchetti@kshell.com>
2024-04-07 11:42:33 -0400

    removed console.log messages

--- test/tests_3d/core_tests/Geometry3d.js
+++ test/tests_3d/core_tests/Geometry3d.js
@@ -9,21 +9,16 @@ describe('Geometry3d', function() {
     it('relativeRotation 1', function (){
         var test = new threejs_math.Vector3(2.0, -1.0, 0.0);
         var angles = manifesto.relativeRotation( test );
-        console.log("angles " + angles);
         
     });
     
     it('relativeRotation 2', function (){
         var test = new threejs_math.Vector3(0.0, 0.0, 1.0);
         var angles = manifesto.relativeRotation( test );
-        console.log("angles " + angles);
-        
     });
     
     it('relativeRotation 3', function (){
         var test = new threejs_math.Vector3(0.0, -3.0, 10.0);
         var angles = manifesto.relativeRotation( test );
-        console.log("angles " + angles);
-        
     });
 });
