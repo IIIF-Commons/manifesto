b3e0ae4d2c4501dd9964ca838a03a82bd84edce0
Vincent Marchetti <vmarchetti@kshell.com>
2024-08-04 16:15:33 -0400

    Added a unit test for the eulerFromRotateTransform which is a low precision test that the resulting camera orientation met the visual requirements. THe plan is to add more exacting tests.

--- test/tests_3d/core_tests/Geometry3d.js
+++ test/tests_3d/core_tests/Geometry3d.js
@@ -94,3 +94,41 @@ describe('Geometry3d: lightRelativeRotation', function() {
         expect( new threejs_math.Vector3().subVectors(light_direction , unit_direction ).length() ).to.be.lessThan(1.0e-4, "light direction not correct"); 
     });
 });
+
+describe('Geometry3d: eulerFromRotateTransform', function() {
+
+    // based on the https://raw.githubusercontent.com/IIIF/3d/whale_anno/manifests/xx_whale_comments/c_comment_annotation_camera.json
+    // manifesto at commit f35a1ad  (27 July 2024)
+    it('c_comment_annotation_camera', function (){
+    
+        var transform = new manifesto.RotateTransform(
+                {
+                    "type": "RotateTransform",
+                    "x": -15.0,
+                    "y": 215.0,
+                    "z": 0.0
+                  }       
+        );
+        
+        var euler = manifesto.eulerFromRotateTransform( transform );
+        
+        expect(euler).to.exist;
+        
+        var neg_z_axis =  new threejs_math.Vector3(0.0,0.0,-1.0);
+        
+        var rotated_z_axis = neg_z_axis.clone().applyEuler(euler);
+        
+        // evaluate the desired direction based on defined camera
+        // position (lookFrom) and target position (lookAt)
+        var lookFrom = new threejs_math.Vector3( -0.25, 0.0, -0.5); 
+        var lookAt = new threejs_math.Vector3(0.040, 0.063, -0.066);
+        
+        var desired_direction = lookAt.clone().sub(lookFrom).normalize();
+        var direction_error = desired_direction.clone().sub(rotated_z_axis);
+        
+        // apply very lax test, since the camera orientation may have been eyeballed
+        // only in a threeJS editor, particularly there may be a depth imprecision.
+        expect( direction_error.length()).to.be.lessThan(1.5e-1, "rotated z direction not correct");
+        
+    });
+});
