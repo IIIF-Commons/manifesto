9b783333ab5aa1f5402c542f018235e6a38ec3d2
Vincent Marchetti <vmarchetti@kshell.com>
2025-06-09 06:31:07 -0400

    Removed expect commands tests that were causing false failures camera tests were testing for an explicit value of fieldOfView property which are not defined in these individual manifests

--- test/tests_3d/2_cameras/positioned_camera_lookat_specific_resource.js
+++ test/tests_3d/2_cameras/positioned_camera_lookat_specific_resource.js
@@ -46,7 +46,7 @@ describe('positioned_camera_lookat_specific_resource', function() {
         expect(camera instanceof manifesto.Camera).to.equal(true);
         expect(camera.isPerspectiveCamera()).to.equal(true);
         expect(camera.isModel()).to.equal(false,"checking isModel()=false");
-        expect(camera.FieldOfView).to.equal(45.0);
+        
         
         let lookedAt = camera.LookAt;
         expect( lookedAt , "find the lookAt annotation.id?").to.exist;
