440b1a213e270240f03a07422e3f0096770b9b28
jamesmisson <jcmisson@gmail.com>
2025-10-22 15:08:32 +0100

    return Annotation[] from AnnotationPage getItems

--- src/AnnotationPage.ts
+++ src/AnnotationPage.ts
@@ -6,6 +6,18 @@ export class AnnotationPage extends ManifestResource {
   }
 
   getItems(): Annotation[] {
-    return this.getProperty("items");
+    const items: Annotation[] = []
+    const rawItems = this.getProperty("items");
+
+    if (!rawItems || !Array.isArray(rawItems)) {
+      return items;
+    }
+    
+    for (let i = 0; i < rawItems.length; i++) {
+      const annotation = new Annotation(i, this.options);
+      items.push(annotation);
+    }
+
+    return items
   }
 }
